17b46788069e260d3183fe39c214e926bb21d711
Cyril Walrond <cyril.d.walrond@gmail.com>
2025-05-13 18:16:40 -0700

    lesson8

--- responsivevictory/day8/dailyChallenge/script.js
+++ responsivevictory/day8/dailyChallenge/script.js
@@ -1,6 +1,13 @@
-let fullName="Cyril Walrond"
+// let fullName = "Cyril Walrond";
+// console.log(fullName.replace("C", "Z"));
+// console.log(fullName.includes("Cyril"));
 
+// console.log("Hello, World" + " " + fullName);
+// console.log(`Hello, World ${fullName}`);
 
-
-console.log("Hello, World"+" "+fullName);
-console.log(`Hello, World ${fullName}`);
+// alert("This is an alert message!");
+// confirm("Are you sure you want to proceed here");
+const userMessage = confirm("Are you sure you want to proceed here");
+console.log(userMessage);
+const favDrink = prompt("What's your favorite drink");
+console.log(favDrink);
